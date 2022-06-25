e4630eab0c430fb65f5430c4f3f72e70e2d1ad23
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-06-25 06:56:22 -0700

    basic_calc

--- calculator.js
+++ calculator.js
@@ -35,7 +35,9 @@ function getNum(num) {
 }
 
 
-
+function equalsToOperator() {
+    getResult(firstNum, secondNum, operator)
+}
 
 
 function getOperator(opr) {
@@ -44,41 +46,28 @@ function getOperator(opr) {
     firstNum = inputVal.value;
 
 
-    if (firstNum != "") {
+    if (firstNum) {
         operator = opr
     }
 
-
-
-
-    if (firstNum && operator && secondNum) {
-        getResult()
-        // console.log(`First num: ${firstNum}`)
-
-        // if (operator) {
-        //     console.log(operator)
-        //     if (secondNum != "") {
-        //         getResult()
-        //         // console.log(`First num: ${firstNum}`)
-        //         // console.log(operator)
-        //         // console.log(`Second num>>: ${secondNum}`)
-        //     }
-        // }
+    if (secondNum) {
+        getResult
     }
-    // if (firstNum != "" && secondNum != "" && operator != "") {
-    //     console.log("bhai kam hojay bharosa rkho")
+    console.log(operator)
 
-    //     getResult()
-    //     // console.log("bhai kam hojay bharosa rkho")
-
-    // }
-    // console.log(`dusra number==>>${secondNum}`)
 }
+
+
+
 
 
 function clearInput() {
     var inputVal = document.getElementById('inputVal');
     var history = document.getElementById('history')
+    firstNum = "";
+    operator = "";
+    secondNum = ""
+    total = ""
     inputVal.value = "";
     history.innerHTML = ""
 }
@@ -87,74 +76,26 @@ function clearInput() {
 
 
 
-function getResult() {
+function getResult(firstNum, secondNum, operator) {
 
     switch (operator) {
         case "+":
             total = Number(firstNum) + Number(secondNum)
+            break;
         case "-":
             total = Number(firstNum) - Number(secondNum)
+            break;
 
         case "*":
             total = Number(firstNum) * Number(secondNum)
+            break;
 
         case "/":
             total = Number(firstNum) / Number(secondNum)
+            break;
     }
-
-    // if (operator === "+") {
-    //     total = Number(firstNum) + Number(secondNum)
-    // }
     inputVal.value = total
     secondNum = ""
-    // console.log("ma get result hon")
-    // console.log(`pehla: ${firstNum}`)
-    // console.log(operator)
-    // console.log(`dusra: ${secondNum}`)
-
-    // var inputVal = document.getElementById('inputVal').value;
-    // var oper = ""
-    // var total = 0
-    // for (var i = 0; i < inputVal.length; i++) {
-    //     if (inputVal[i] === "+" || inputVal[i] === "-" || inputVal[i] === "*" || inputVal[i] === "/") {
-
-    //         oper = inputVal[i];
-    //         console.log(oper)
-    //         var splitted = inputVal.split(oper)
-    //         numberArray.push(splitted)
-    //         console.log(splitted)
-    //     }
-    // }
-    // var num1 = Number(numberArray[0][0]);
-    // var num2 = Number(numberArray[0][1]);
-    // // calculator-7e3b5
-
-    // if (oper === "+") {
-    //     total = num1 + num2
-    // }
-    // else if (oper === "-") {
-    //     total = num1 - num2
-    // }
-
-    // else if (oper === "*") {
-    //     total = num1 * num2
-    // }
-
-    // else if (oper === "/") {
-    //     total = (num1 / num2).toFixed(2)
-    // }
-    // else if (oper === "%") {
-    //     total = num1 % num2
-    // }
-    // console.log(num2)
-
-
-    // var inputVal2 = document.getElementById('inputVal');
-
-    // inputVal2.value = total;
-    // console.log(total)
-
-    // numberArray = []
 }
 
 function back() {
